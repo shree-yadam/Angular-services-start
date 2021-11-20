459f9ba9fabb6951730f1fb0c6ef5d2c6c47573c
Shree Devi Yadam <shree.yadam@gmail.com>
2021-11-20 12:52:33 -0500

    Data Service Hierrchical Injection

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { AccountsService } from '../shared/accounts.service';
+import { LoggingService } from '../shared/logging.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private loggingService: LoggingService,
+    private accountsService: AccountsService) {}
+
+  onSetTo(status: string) {
+    this.accountsService.updateStatus(this.id, status);
+    this.loggingService.logStatusChange(status);
+  }
+}
---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AccountsService } from '../shared/accounts.service';
+import { LoggingService } from '../shared/logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
+})
+export class NewAccountComponent {
+
+
+  constructor(private loggingService: LoggingService,
+    private accountsService: AccountsService) {}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.loggingService.logStatusChange(accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);
+  }
+}
